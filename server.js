66eac37f55d2854d4aeca7099dcdbb1dd4825196
Nick <nick.win999@gmail.com>
2018-11-10 16:52:40 -0600

    Add sprites to static

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const routing = require('./routing.js')(app);
 
 app.use('/static', express.static('static'));
 app.use('/img', express.static('Pokemon-DB/img'));
+app.use('/spr', expres.static('./Pokemon-DB/spr'));
 
 const server = new http.Server(app);
 
