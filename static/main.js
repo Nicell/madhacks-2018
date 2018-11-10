0f75a93fa65e612d61c4a27d15cda9fe32346aa1
Nick <nick.win999@gmail.com>
2018-11-10 15:29:44 -0600

    More styling

--- static/main.js
+++ static/main.js
@@ -12,8 +12,12 @@ const search = () => {
         }, (err, content) => {
             console.log(content);
             for (const hit of content.hits) {
-                const ename = hit._highlightResult.ename ? hit._highlightResult.ename.value : hit.ename;
-                hits += `<span>${ename}</span>`;
+                const ename = `<span>${hit._highlightResult.ename.value}</span>`;
+                let types = '';
+                for (const type of hit._highlightResult.type) {
+                    types += `<span>${type.value}</span>`;
+                }
+                hits += `<div class="hit">${ename}<div class="types">${types}</div></div>`;
             }
             document.getElementById('hits').innerHTML = hits;
         });
