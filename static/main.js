35f7c0f28dcd63b8ee64789888269453cea2315a
Jack Brud <jack.brud2530@gmail.com>
2018-11-10 15:57:54 -0600

    color changes to types

--- static/main.js
+++ static/main.js
@@ -15,7 +15,8 @@ const search = () => {
                 const ename = `<span>${hit._highlightResult.ename.value}</span>`;
                 let types = '';
                 for (const type of hit._highlightResult.type) {
-                    types += `<span>${type.value}</span>`;
+                    const cleanType = type.value.replace('<em>', '').replace('</em>', '');
+                    types += `<span class="${cleanType.toLowerCase()}">${type.value}</span>`;
                 }
                 hits += `<div class="hit">${ename}<div class="types">${types}</div></div>`;
             }
