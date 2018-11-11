73ef643a69884f0c40a1a9d83118c3fb1be06413
Nick <nick.win999@gmail.com>
2018-11-11 07:01:20 -0600

    get data

--- static/generation.js
+++ static/generation.js
@@ -3,8 +3,41 @@ const index = client.initIndex('dev_pokedex');
 
 const id = location.pathname.split('/').pop();
 
+const searchParameters = {
+    1: [
+        'idInt <= 151'
+    ],
+    2: [
+        'idInt >= 152',
+        'idInt <= 251'
+    ],
+    3: [
+        'idInt >= 252',
+        'idInt <= 386'
+    ],
+    4: [
+        'idInt >= 387',
+        'idInt <= 493'
+    ],
+    5: [
+        'idInt >= 494',
+        'idInt <= 649'
+    ],
+    6: [
+        'idInt >= 650'
+    ]
+}
+
 const gens = () => {
     const generation = document.getElementById('gens').value;
 
     window.location.href = 'http://' + window.location.host + '/gen/' + generation;
 }
+
+index.search({
+    query: '',
+    numericFilters: searchParameters[id],
+    hitsPerPage: 200
+}, (err, content) => {
+    console.log(content.hits);
+});
