2c4d3d210b04b9116fb2b56feb8f5be490f8ee12
Jack Brud <jack.brud2530@gmail.com>
2018-11-10 18:31:12 -0600

    big boy commits (html, profilecss/js)

--- static/profile.js
+++ static/profile.js
@@ -6,5 +6,27 @@ const id = location.pathname.split('/').pop();
 index.search({
     query: id
 }, (err, content) => {
-    console.log(content.hits[0]);
+    const pokemon = content.hits[0];
+    const pkmName = pokemon.ename.charAt(0).toUpperCase() + pokemon.ename.slice(1);
+
+    document.getElementById("favicon").setAttribute("href", `/spr/${id}${pkmName}.png`)
+    let types = "";
+    for (const type of pokemon.type) {
+        types += `<span class="${type}">${type}</span>`;
+    }
+
+    const template = `
+        <div class="identity"> 
+            <img src="/img/${id}${pkmName}.png"/>
+            <span>${pkmName}</span>
+            <div class="types">${types}</div>
+        </div>
+
+        <div class="stats">
+
+        </div>
+    `;
+
+    document.getElementById("profile").innerHTML = template;
 });
+
